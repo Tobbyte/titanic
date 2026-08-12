ce45ff1b382893f3c26d43d7a2fc621c837b8fe1
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-08-12 11:50:13 +0200

    use v 2.1.1

--- fuzzy_tobbyte.py
+++ fuzzy_tobbyte.py
@@ -5,7 +5,7 @@ Limitations:
     - rudimentary sorting order of matches.
 
 
-Version: 2.1.0
+Version: 2.1.1
 
 ***********
 Copy of module standalones/tobbyte_fuzzy/
@@ -13,8 +13,6 @@ Copy of module standalones/tobbyte_fuzzy/
 
  ~ Made with ❤️ and without ai or code completion (except intelliSense) ~
 """
-
-__all__ = ["get_similar"]  # public method
 
 FUZZY_DIST_DEFAULT = 2
 
@@ -81,6 +79,7 @@ def get_similar(
     Does not consider capitalization.
     Looks for direct matches and fuzzy matches of any part of the search
     term in any part of the db items.
+    Returns full, direct matches directly and stops searching further.
     The fuzzy search only considers parts of the db items in the length
     of the length of the original search term parts to better match
     small typos.
@@ -104,11 +103,12 @@ def get_similar(
             len(word) == FUZZY_DIST_DEFAULT (+ x ?). So that no longer
             "ab" matches "cd". require first char the same? tbd.
         - tbd: split on special chars like "-"?
+        - return first full match or always fuzzy by param
 
     """
-    search_term_split = dissect_string(search_term)[search_term]
+    search_term_split: list[str] = dissect_string(search_term)[search_term]
     matches = {}
-    already_matched_st = []
+    already_matched_st: list[str] = []
     already_matched_ip = []
 
     for item in db:
@@ -142,6 +142,11 @@ def get_similar(
                             ),
                         )
                         already_matched_ip.append(item_part)
+
+        if set(search_term_split) <= set(already_matched_st):
+            # all search term parts matched directly db item.
+            # no need to look further (except flag thats tbd.)
+            return [item]
 
         # fill with Nones for item_parts not matched direct or fuzzy.
         # Used for sorting order of results.
